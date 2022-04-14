db8ca60a0ea3accb0c5daed1a553368b21b15d16
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2022-04-14 01:41:00 -0400

    board kinda resizes

--- src/components/Letter.js
+++ src/components/Letter.js
@@ -1,5 +1,38 @@
 import React, { useContext, useEffect } from "react";
 import { AppContext } from "../App";
+import styled from 'styled-components'
+import theme from "../theme";
+
+const LetterWrapper = styled.div`
+  flex: 1;
+  border: 1px solid grey;
+  margin: 0px 5px;
+  display: grid;
+  place-items: center;
+  font-size: 40px;
+  font-weight: bolder;
+  color: ${theme.colors.black};
+  font-family: Arial, Helvetica, sans-serif;
+  border-radius: 2px;
+
+  &.correct {
+    background-color: #B71000;
+    color: white;
+    border: 0px;
+  }
+  
+  &.almost {
+    background-color: #EFC3C0;
+    color: white;
+    border: 0px;
+  }
+  
+  &.error {
+    background-color: #74A2A5;
+    color: white;
+    border: 0px;
+  }
+`
 
 function Letter({ letterPos, attemptVal }) {
   const { board, setDisabledLetters, currAttempt, correctWord } =
@@ -19,9 +52,9 @@ function Letter({ letterPos, attemptVal }) {
     }
   }, [currAttempt.attempt]);
   return (
-    <div className="letter" id={letterState}>
+    <LetterWrapper className={letterState}>
       {letter}
-    </div>
+    </LetterWrapper>
   );
 }
 
